ede4e08b1f00aa5b64fe4c8cfaedd8cc44533527
isohamhalbandge <logzii123@gmail.com>
2023-12-21 11:39:50 +0530

    test_api done

--- project2/testapi.py
+++ project2/testapi.py
@@ -5,9 +5,17 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 # Importing html file
-@app.route('/')
+@app.route('/') # '/' -> home folder or directory
 def show_form():
     return render_template('index.html')
+
+# checking user details
+@app.route("/process_form", methods=['POST'])
+def check_userdetails():
+    name = request.form.get('username')
+    password = request.form.get('password')
+    print({name}, {password})
+    return "username and password recieved"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
